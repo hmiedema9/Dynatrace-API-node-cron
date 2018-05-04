37a470b1457185eb37fcd5df44411fee1a50968b
Hayden Miedema <haydenmiedema@gmail.com>
2018-05-03 20:21:26 -0700

    added request and cron. boilerplate for some DT variables

--- cron.js
+++ cron.js
@@ -1,7 +1,17 @@
+var request = require('request');
 var CronJob = require('cron').CronJob;
+
+// Setup DT variables for API
+var TENANT_ID = "";
+var DT_SAAS = ""; // can be used later for URL manipulation
+var TENANT_TOKEN = "";
+
 var job = new CronJob({
-  cronTime: '* * * * *',
+  cronTime: '0 0 */2 * *', 
   onTick: function() {
+      /* 
+       * This job will be run at 00:00 every 2nd day (48 hours)
+       */
     console.log('test cron');
   },
   start: false,
