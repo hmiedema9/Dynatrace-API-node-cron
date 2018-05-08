b94323015593df968b021058b4fa78ef481e47b2
Hayden Miedema <haydenmiedema@gmail.com>
2018-05-07 23:54:27 -0700

    add support for json read from file

--- cron.js
+++ cron.js
@@ -2,11 +2,13 @@
 var request = require('request');
 // 'cron' npm package being used for recurring jobs - https://www.npmjs.com/package/cron
 var CronJob = require('cron').CronJob;
+var fs = require("fs");
 
 // Setup DT variables for API - grab from env variable needs to be configured
 var TENANT_ID = process.env.TENANT_ID;
 var DT_SAAS = ""; // can be used later for URL manipulation
 var TENANT_TOKEN = process.env.TENANT_TOKEN;
+var DEVICE_DETAILS = fs.readFileSync("details.json");
 
 var job = new CronJob({
   cronTime: '0 0 */2 * *', 
@@ -16,16 +18,7 @@ var job = new CronJob({
        */
     request.post(
       'https://dynatrace-paas.appl.kp.org/e/'+ TENANT_ID + '/api/v1/entity/infrastructure/custom/DataPowerLoadBalancerQA?Api-Token=' + TENANT_TOKEN,
-      { json: {
-        "displayName" : "Service Bus QA",
-        "ipAddresses" : ["172.20.49.21"],
-        "listenPorts" : ["9999"],
-        "type" : "F5-LoadBalancer",
-        "favicon" : "http://assets.dynatrace.com/global/icons/f5.png",
-        "configUrl" : "http://172.20.49.21",
-        "tags": ["F5", "DataPower"],
-        "properties" : { "environment" : "QA" }
-      } },
+      { json: DEVICE_DETAILS },
       function (error, response, body) {
           if (!error && response.statusCode == 200) {
               console.log(body)
